4983ab1f38a1bbb3639be51b45f383bfcfd13c64
Ethan Zerba <ekzerba@gmail.com>
2023-03-20 20:45:49 -0600

    hotfix

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb1/1-1/1-1.2/1-1.2:1.0/ttyUSB0/tty/ttyUSB0',
+                'serial_port': '/dev/ttyUSB0',
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
