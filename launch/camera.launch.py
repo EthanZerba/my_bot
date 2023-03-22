7f910697c4a3de06096bfaa2619f6a0858d636da
Ethan Zerba <ekzerba@gmail.com>
2023-03-21 20:28:16 -0600

    changes files

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
             executable='v4l2_camera_node',
             output='screen',
             parameters=[{
-                'image_size': [640,480],
+                'image_size': [1920,1080],
                 'camera_frame_id': 'camera_link_optical'
                 }]
     )
