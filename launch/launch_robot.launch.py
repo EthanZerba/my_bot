34302cbae8a83d472216f78588689040c5dbc6c5
ekzerba <ekzerba@gmail.com>
2023-04-13 20:23:10 -0600

    re added stuff

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -79,6 +79,28 @@ def generate_launch_description():
         )
     )
 
+    camera = Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            output='screen',
+            parameters=[{
+                'image_size': [480,360],
+                'camera_frame_id': 'camera_link_optical'
+                }]
+    )
+
+    lidar = Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard',
+            }]
+        )
+
 
     
     return LaunchDescription([
@@ -86,5 +108,7 @@ def generate_launch_description():
         twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        delayed_joint_broad_spawner,
+        camera,
+        lidar,
     ])
